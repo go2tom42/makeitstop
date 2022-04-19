63f9ab3297e1390d9d5fbd69caae4be7533b9321
go2tom42 <tom42martin@gmail.com>
2022-04-19 14:11:20 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 while true; do echo n; done | cp -Ri /config-default/* /config/
 exec su-exec root ls /
-exec su-exec root ls /config
+exec su-exec root ls /config/
 exec su-exec root /config/watch.sh
