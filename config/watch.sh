de481658c07c6017ddccf5214490fd8cbdce66db
go2tom42 <tom42martin@gmail.com>
2022-04-19 20:13:35 -0400

    Update watch.sh

--- config/watch.sh
+++ config/watch.sh
@@ -2,9 +2,10 @@
 MONITORDIR1="/media/film-in/"
 MONITORDIR2="/media/tv-in/"
 
-inotifywait -re close_write  -m --excludei '.*sample\.(avi|mkv|mp4)|.*NORMALIZED\.mkv' --format '%w%f' $MONITORDIR1 $MONITORDIR2 |
+inotifywait -re close_write  -m --excludei '.*sample\.(avi|mkv|mp4)|.*NORMALIZED\.mkv|.*_unpack.*' --format '%w%f' $MONITORDIR1 $MONITORDIR2 |
     while read NEWFILE; do
         if [[ "$NEWFILE" =~ '.*\.(avi|AVI|MKV|mkv|mp4|MP4)' ]]; then # Does the file end with .xml?
             su-exec 1000:1000 pwsh /config/normalize.ps1 "$NEWFILE" &
         fi
     done
+
